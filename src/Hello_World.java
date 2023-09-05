066f74087887653a86bec6207a2aabc6cc1fb5ec
KFitz1123 <Kfitz53090@gmail.com>
2023-09-05 17:58:50 -0500

    Initial commit

---
+++ src/Hello_World.java
@@ -0,0 +1,11 @@
+import java.io.PrintWriter;
+
+public class Hello_World {
+  public static void main (String[] args) throws Exception{
+PrintWriter pen = new PrintWriter(System.out, true);
+pen.println("HelloWorld");
+pen.flush();
+
+
+  }//main
+}// Class Hello_World 
